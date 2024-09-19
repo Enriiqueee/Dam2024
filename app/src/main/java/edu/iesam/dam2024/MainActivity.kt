f5d772b3b81b5d767e645ed6ede3e18bfb292e4a
enriiquee <kikiarenas46@gmail.com>
2024-09-19 19:49:42 +0200

    Primera subida al repositorio

--- app/src/main/java/edu/iesam/dam2024/MainActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.iesam.dam2024
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +9,19 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private var name : String? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //Scope functions
+        name  = "Alumno"
+        name?.let { nameNotNull ->
+            Log.d("@dev", nameNotNull)
         }
     }
 }
