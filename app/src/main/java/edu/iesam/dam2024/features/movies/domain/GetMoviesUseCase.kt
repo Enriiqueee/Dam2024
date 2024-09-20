753a0ef8cf82c5b032b6a8fd5830781164f0b41e
enriiquee <kikiarenas46@gmail.com>
2024-09-20 18:22:39 +0200

    Creada la de movie y comienzo de superhero

---
+++ app/src/main/java/edu/iesam/dam2024/features/movies/domain/GetMoviesUseCase.kt
@@ -0,0 +1,12 @@
+package edu.iesam.dam2024.features.movies.domain
+
+/**
+ * Naming: Verbo(Acción) + UseCase
+ */
+
+class GetMoviesUseCase(private val movieRepository: MovieRepository){
+
+    operator fun invoke(): List<Movie>{
+        return movieRepository.getMovies()
+    }
+}
