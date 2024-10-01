0a9e8253a3ee9eac46076ee30d21effed3bb0d44
Enrique <arenasvinasenrique@gmail.com>
2024-10-01 17:50:51 +0200

    -Añadido detalle de la vista

--- app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MoviesViewModel.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MoviesViewModel.kt
@@ -6,16 +6,9 @@ import edu.iesam.dam2024.features.movies.domain.GetMoviesUseCase
 import edu.iesam.dam2024.features.movies.domain.Movie
 
 class MoviesViewModel(
-    private val getMoviesUseCase: GetMoviesUseCase,
-    private val getMovieUseCase: GetMovieUseCase
-) : ViewModel() {
+    private val getMoviesUseCase: GetMoviesUseCase) : ViewModel() {
 
     fun viewCreated(): List<Movie> {
         return getMoviesUseCase.invoke()
     }
-
-    fun itemSelected(movieId: String): Movie? {
-        return getMovieUseCase.invoke(movieId)
-    }
-
 }
