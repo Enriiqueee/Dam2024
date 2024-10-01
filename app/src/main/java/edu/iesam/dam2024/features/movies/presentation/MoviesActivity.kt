193cac139da2b8c313afd399a460c71c89aded42
Enrique <arenasvinasenrique@gmail.com>
2024-10-01 16:29:12 +0200

    -Renombrado de clases

--- app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MoviesActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MoviesActivity.kt
@@ -9,23 +9,21 @@ import edu.iesam.dam2024.R
 import edu.iesam.dam2024.features.movies.data.local.MovieXmlLocalDataSource
 import edu.iesam.dam2024.features.movies.domain.Movie
 
-class MovieActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity() {
 
     private lateinit var movieFactory: MovieFactory
     private lateinit var viewModel : MovieViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies)
 
         movieFactory = MovieFactory(this)
         viewModel = movieFactory.buildViewModel()
 
         val movies = viewModel.viewCreated()
         bindData(movies)
-        val movie = viewModel.itemSelected(movies.first().id)
-        //testListXml()
-        testMovie()
+
     }
 
     private fun testMovie(){
