b2d2450a9496c7373bdf010164df3ed492945655
Enrique <arenasvinasenrique@gmail.com>
2024-10-02 17:39:30 +0200

    -Uso de la clase ImageViewExt.kt para introducir el glide alli

--- app/src/main/java/edu/iesam/dam2024/app/extensions/ImageViewExt.kt
+++ app/src/main/java/edu/iesam/dam2024/app/extensions/ImageViewExt.kt
@@ -3,10 +3,10 @@ package edu.iesam.dam2024.app.extensions
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
-        fun ImageView.loadUrl(urlImg: String) {
+        fun ImageView.loadUrl(url: String) {
 
             Glide
                 .with(this)
-                .load(urlImg)
+                .load(url)
                 .into(this)
         }
